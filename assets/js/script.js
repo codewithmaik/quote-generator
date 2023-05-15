b373b03f51cf89f45f689e995b402a0bc25fcdc1
codewithmaik <mcawelius@web.de>
2023-05-15 16:11:13 +0000

    Add getQuotes() function to get quotes from API

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let apiQuotes = [];
+
+// GET QUOTES FROM API
+async function getQuotes() {
+    const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+    } catch (error) {
+        // CATCH ERROR HERE
+    }
+}
