cd364bfff3c6c8584b9b719a6a855941803e477b
codewithmaik <mcawelius@web.de>
2023-05-16 08:11:46 +0000

    Extend getRandomQuote() function to display quote and adjust styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote-text');
-const quoteAuthor = document.getElementById('author-text');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
 
@@ -8,7 +8,22 @@ let apiQuotes = [];
 
 // GET RANDOM QUOTE
 function getRandomQuote() {
+    // PICK A RANDOM QUOTE FROM APIQUOTES
     let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // CHECK IF AUTHOR FIELD IS EMPTY AND REPLACE IT WITH UNKNOWN
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // CHECK QUOTE LENGTH TO DETERMINE STYLING
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
 }
 
 // GET QUOTES FROM API
