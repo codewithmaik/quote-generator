9b3a14d7d323e6c08dd5c032c89ee7d51ca0701c
codewithmaik <mcawelius@web.de>
2023-05-16 07:22:26 +0000

    Add getRandomQuote() function to pick random quote out of apiQuotes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 let apiQuotes = [];
 
+// GET RANDOM QUOTE
+function getRandomQuote() {
+    let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+}
+
 // GET QUOTES FROM API
 async function getQuotes() {
     const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
+        getRandomQuote();
     } catch (error) {
         // CATCH ERROR HERE
     }
